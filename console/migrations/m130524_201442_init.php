280338f94f030d7ca78e401981d9f20e14b35170
dickens038 <dickensmanyama*@gmail.com>
2025-07-21 17:54:24 +0300

    Recreate clean migration file with no require_once for successful migration run

---
+++ console/migrations/m130524_201442_init.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\db\Migration;
+
+class m130524_201442_init extends Migration
+{
+    public function up()
+    {
+        // ... migration code ...
+    }
+
+    public function down()
+    {
+        // ... migration code ...
+    }
+}
