792cb741945e6b22699236ab06f953ae3b622afd
Dickens Manyama <dickensmanyama8@gmail.com>
2025-07-23 16:57:51 +0300

    Update main.php

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -1,9 +1,4 @@
-// Add this inside the 'components' array in your config
-'response' => [
-    'on beforeSend' => function ($event) {
-        $event->sender->headers->add('Content-Security-Policy', "default-src 'self'; img-src 'self' data:;");
-    },
-],
+
 <?php
 $params = array_merge(
     require __DIR__ . '/../../common/config/params.php',
